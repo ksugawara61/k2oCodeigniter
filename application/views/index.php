db008586942681879d068515079313b45bd1e640
ksugawara61 <katsuo.com61@gmail.com>
2017-06-08 23:37:11 +0900

    トップページの画像を移管

--- application/views/index.php
+++ application/views/index.php
@@ -45,14 +45,14 @@ $(document).ready(function(){
                     <h1>バッタになりたい魔法使い</h1>
                     <p>コモモ・ひろましゃ制作のWeb仕掛け絵本<br>合い言葉は、バッタになっちゃえ！</p>
                 </div>
-                <img src="images/mainVisual_img_03.jpg" width="980" height="500" alt="">
+                <img src="/assets/images/mainVisual_img_03.jpg" width="980" height="500" alt="">
             </li>
             <li>
                 <div class="mainVisualText">
                     <h1>WordPressデザインワークブック</h1>
                     <p>コモモとひろましゃ二人の共著によるWordPressで本格的なウェブサイトを制作したい方向けの書籍</p>
                 </div>
-                <img src="images/mainVisual_img_02.jpg" width="980" height="500" alt="">
+                <img src="/assets/images/mainVisual_img_02.jpg" width="980" height="500" alt="">
             </li>
             <li>
                 <div class="mainVisualText">
@@ -60,26 +60,26 @@ $(document).ready(function(){
                     <p>baserCMS、カフェサイト用テーマ<br>
                         baserCMS2012 テーマコンテスト 飲食店系テーマ賞受賞</p>
                 </div>
-                <img src="images/mainVisual_img_01.jpg" width="980" height="500" alt="">
+                <img src="/assets/images/mainVisual_img_01.jpg" width="980" height="500" alt="">
             </li>
         </ul>
     </section>
 
     <section class="portfolioIndex">
         <article>
-            <a href="portfolio/03.html"><img src="portfolio/images/index_03.jpg" width="300" height="163" alt=""></a>
+            <a href="portfolio/03.html"><img src="/assets/images/portfolio/index_03.jpg" width="300" height="163" alt=""></a>
             <h2><a href="portfolio/03.html">バッタになりたい魔法使い</a></h2>
             <p>コモモ・ひろましゃ制作のWeb仕掛け絵本<br>合い言葉は、バッタになっちゃえ！</p>
         </article>
 
         <article>
-            <a href="portfolio/02.html"><img src="portfolio/images/index_02.jpg" width="300" height="163" alt=""></a>
+            <a href="portfolio/02.html"><img src="/assets/images/portfolio/index_02.jpg" width="300" height="163" alt=""></a>
             <h2><a href="portfolio/02.html">WordPressデザインワークブック</a></h2>
             <p>コモモとひろましゃ二人の共著によるWordPressで本格的なウェブサイトを制作したい方向けの書籍</p>
         </article>
 
         <article>
-            <a href="portfolio/01.html"><img src="portfolio/images/index_01.jpg" width="300" height="163" alt=""></a>
+            <a href="portfolio/01.html"><img src="/assets/images/portfolio/index_01.jpg" width="300" height="163" alt=""></a>
             <h2><a href="portfolio/01.html">Cafe Debut</a></h2>
             <p>baserCMSのカフェサイト用汎用テーマ<br>テーマコンテスト2012飲食店系テーマ賞受賞</p>
         </article>
